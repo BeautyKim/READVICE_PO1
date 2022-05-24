0ecfeb4418f8eba999235834fb85958714703cfe
HAJINHEE <hajinheee@gmail.com>
2022-05-24 16:52:19 +0900

    이미지 간격

--- components/Team.js
+++ components/Team.js
@@ -8,37 +8,40 @@ export function Team() {
                     Big company announcement or simple sub-header taking two or more lines.</h5>
             </div>
             <div className="imgAll">
-            <div className="img1">
-                <img
+            <div className="img1" align = "center">
+                <img hspace ="20"
                     width='200px'
                     height='200px'
                     src='https://www.w3schools.com/howto/img_avatar2.png'></img>
                 <h4>Ah-Reum Kim</h4>
-                <h6>사장입니다. 성실합니다.
+                <h6>사장입니다.<br/> 
+                성실합니다.
                 </h6>
             </div>
-            <div className="img2">
+            <div className="img2" align = "center">
                 <img
                     width='200px'
                     height='200px'
                     src='https://www.w3schools.com/howto/img_avatar2.png'></img>
-                <h4>Hong-ju Noh</h4>
-                <h6>셋째입니다. 매의 눈을 담당하고 있습니다.</h6>
+                <h4 >Hong-ju Noh</h4>
+                <h6>셋째입니다.<br/>
+                매의 눈을 담당하고 있습니다.</h6>
             </div>
-            <div className="img3">
-                <img
+            <div className="img3" align = "center">
+                <img hspace ="20"
                     width='200px'
                     height='200px'
                     src='https://www.w3schools.com/howto/img_avatar2.png'></img>
                 <h4>Min-seo Choi</h4>
-                <h6>막내입니다. 요정을 담당하고 있습니다.</h6>
+                <h6>막내입니다. <br/>
+                요정을 담당하고 있습니다.</h6>
             </div>
-            <div className="img4">
-                <img
+            <div className="img4" align = "center">
+                <img 
                     width='200px'
                     height='200px'
                     src='https://www.w3schools.com/howto/img_avatar2.png'></img>
-                <h4>Jin-hee Ha</h4>
+                <h4 >Jin-hee Ha</h4>
                 <h6>둘째입니다.</h6>
             </div>
             </div>
@@ -46,7 +49,6 @@ export function Team() {
                 .imgAll {
                     float: right;
                     display: flex;
-                    
                 }
             `}</style>
         </form>
